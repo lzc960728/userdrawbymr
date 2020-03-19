9c7d103988f80f5c0daf4eab73d4a5808ac275e7
lzc960728 <793639877@qq.com>
2020-03-19 14:52:09 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,5 @@
+ git init
+                  git add README.md
+                  git commit -m "first commit"
+                  git remote add origin git@github.com:lzc960728/userdrawbymr.git
+                  git push -u origin master
---
+++ src/main/java/mr/UserDrawStepOneMapReduce.java
@@ -0,0 +1,15 @@
+package mr;
+
+/**
+ * @program: userdrawbymr
+ * @ClassName UserDrawStepOneMapReduce
+ * @description:
+ * @author: lzc
+ * @create: 2020-03-19 14:44
+ * @Version 1.0
+ **/
+public class UserDrawStepOneMapReduce {
+
+
+
+}
